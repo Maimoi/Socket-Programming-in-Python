d14b43c2988ab52f3668d64b156d0e8e1e491810
Maimoi <himohit1990@gmail.com>
2020-10-08 17:05:25 +0530

    Added comments to the both server and client files

--- server1.py
+++ server1.py
@@ -1,10 +1,24 @@
+#import the socket module
 import socket
+
+#create a socket object 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#bind the servert to the client 
 s.bind((socket.gethostbyname('localhost'),1024))
+
+#listen to the client 
 s.listen(5)
+
+
 while True:
+
     client_socket, address = s.accept()
     print(f"Conncetion established  to {address} successfully!")
+
+
     client_socket.send(bytes("Socket Programming in Python", "utf-8"))
+
+    #close the connection
     client_socket.close()
 
